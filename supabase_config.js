b155c83bf3952eb47a42ac178b61cf43804b36c4
teacherflavius <handinhandengl@gmail.com>
2026-04-26 07:49:44 -0300

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "COLE_AQUI_A_URL_DO_SEU_PROJETO_SUPABASE",
-  anonKey: "COLE_AQUI_A_CHAVE_PUBLICA_ANON_DO_SUPABASE"
+  url: "https://wnigzpvgsbpjdxvjzugt.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduaWd6cHZnc2JwamR4dmp6dWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxOTk1NzUsImV4cCI6MjA5Mjc3NTU3NX0.q2Mp8bPD4WvOjifSuQFfrAM4ig1ViEa6sMfUNcES-X0"
 };
