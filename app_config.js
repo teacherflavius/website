cf0adaadc81007fa69faac1a683dc617549c7dae
teacherflavius <handinhandengl@gmail.com>
2026-04-30 23:40:01 -0300

    Add shared app configuration

---
+++ app_config.js
@@ -0,0 +1,11 @@
+window.APP_CONFIG = Object.freeze({
+  professorAdminEmail: "flaviofreitas@ufu.br",
+  paths: Object.freeze({
+    home: "/",
+    login: "/login/",
+    professor: "/professor/"
+  }),
+  whatsapp: Object.freeze({
+    teacher: "5534998349756"
+  })
+});
